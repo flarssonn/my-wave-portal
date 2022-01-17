b726535e345a3a57eea158a87149c2652eedb688
Filip Larsson <filip@Filips-MacBook-Pro.local>
2022-01-17 08:21:45 +0100

    Deploy and run scripts for smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -18,4 +18,10 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.METAMASK_PRIVATE_KEY]
+    }
+  }
 };
